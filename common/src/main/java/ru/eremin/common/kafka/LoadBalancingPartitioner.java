bcd64a96c1a6bd6a0ec25cd92b564ead0ed9f7ca
Viktor Eremin <vektor451@gmail.com>
2026-09-23 02:27:22 +0300

    log4 kafka balancer

--- common/src/main/java/ru/eremin/common/kafka/LoadBalancingPartitioner.java
+++ common/src/main/java/ru/eremin/common/kafka/LoadBalancingPartitioner.java
@@ -1,5 +1,6 @@
 package ru.eremin.common.kafka;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.producer.Partitioner;
 import org.apache.kafka.common.Cluster;
 import org.apache.kafka.common.utils.Utils;
@@ -7,8 +8,9 @@ import org.apache.kafka.common.utils.Utils;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 
-public class LoadBalancingPartitioner implements Partitioner {
 
+@Slf4j
+public class LoadBalancingPartitioner implements Partitioner {
     private final AtomicInteger counter = new AtomicInteger();
 
     @Override
@@ -20,26 +22,28 @@ public class LoadBalancingPartitioner implements Partitioner {
                          Cluster cluster) {
 
         int numPartitions = cluster.partitionCountForTopic(topic);
+        int partition;
 
         if (numPartitions <= 1) {
-            return 0;
-        }
-
-        if (keyBytes == null || keyBytes.length == 0) {
+            partition = 0;
+        } else if (keyBytes == null || keyBytes.length == 0) {
             int next = counter.getAndIncrement();
-            return (next & Integer.MAX_VALUE) % numPartitions;
+            partition = (next & Integer.MAX_VALUE) % numPartitions;
+        } else {
+            partition = Utils.toPositive(Utils.murmur2(keyBytes)) % numPartitions;
         }
 
-        return Utils.toPositive(Utils.murmur2(keyBytes)) % numPartitions;
+        log.debug("[LoadBalancingPartitioner] topic={} key={} -> partition={} of {}",
+                topic, key, partition, numPartitions);
+
+        return partition;
     }
 
     @Override
     public void configure(Map<String, ?> configs) {
-
     }
 
     @Override
     public void close() {
-
     }
 }
